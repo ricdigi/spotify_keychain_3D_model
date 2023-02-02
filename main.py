85349a5967734e5e35c7b801a3805470c106ae10
Dodo0303 <dodo0303@icloud.com>
2023-02-02 18:00:10 +0100

    Slight improvements in robustness: image only gets saved if it's not empty.

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ URL = "https://www.spotifycodes.com/downloadCode.php?uri=jpeg%2F000000%2Fwhite%2
 if __name__ == '__main__':
 
     share_link = input("Enter link of song, album, artist or user: ")
+
     data = utils.get_link_data(share_link)
 
+    if len(data) != 2:
+        print("Something went wrong. Probably your fault.")
+        exit(-1)
 
     code_URL = "https://www.spotifycodes.com/downloadCode.php?uri=jpeg%2F000000%2Fwhite%2F640%2Fspotify%3A" + data[0] + "%3A" + data[1]
 
     r = requests.get(code_URL)
 
+    if not r.ok or not r.content:
+        print("Something went wrong. Probably your fault.")
+        exit(-1)
+
     with open(data[1]+".png", "wb") as fp:
         fp.write(r.content)
+
+
