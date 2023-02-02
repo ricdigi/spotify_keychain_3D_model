5ee1a3c59bead68419e33fe43542c9f075effb9b
Dodo0303 <dodo0303@icloud.com>
2023-02-02 20:57:17 +0100

    Added image recognition.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import utils
 import requests
-
+from PIL import Image
 
 URL = "https://www.spotifycodes.com/downloadCode.php?uri=jpeg%2F000000%2Fwhite%2F640%2Fspotify%3Aalbum%3A4m2880jivSbbyEGAKfITCa"
 
@@ -22,7 +22,34 @@ if __name__ == '__main__':
         print("Something went wrong. Probably your fault.")
         exit(-1)
 
+    #img = Image.open(io.BytesIO(r.content))
+
     with open(data[1]+".png", "wb") as fp:
         fp.write(r.content)
 
+    img = Image.open(data[1]+".png").crop((160,0, 640-31, 160))
+    width, height = img.size
 
+    pix = img.load()
+
+    bar_heights = []
+    max_height_of_single_bar = 0
+
+    for x in range(width):
+
+        at_bar = False
+        curr_height = 0
+
+        for y in range(height):
+            if pix[x,y][0] > 20 or pix[x,y][1] > 20 or pix[x,y][2] > 20:
+                at_bar = True
+                curr_height += 1
+
+        if at_bar and curr_height > max_height_of_single_bar:
+            max_height_of_single_bar = curr_height
+        elif not at_bar and max_height_of_single_bar > 0:
+            bar_heights.append(max_height_of_single_bar)
+            max_height_of_single_bar = 0
+
+
+    print(f"There are {len(bar_heights)} bars of heights {bar_heights}")
