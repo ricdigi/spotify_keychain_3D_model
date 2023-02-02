b719f43bd4d09fd44cf1200a15b61e4b5c6bd814
Dodo0303 <dodo0303@icloud.com>
2023-02-02 17:45:16 +0100

    Added image downloader. Input a share link and the file {URI}.png is added to the project folder.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import utils
+import requests
 
 
 URL = "https://www.spotifycodes.com/downloadCode.php?uri=jpeg%2F000000%2Fwhite%2F640%2Fspotify%3Aalbum%3A4m2880jivSbbyEGAKfITCa"
@@ -6,20 +7,12 @@ URL = "https://www.spotifycodes.com/downloadCode.php?uri=jpeg%2F000000%2Fwhite%2
 if __name__ == '__main__':
 
     share_link = input("Enter link of song, album, artist or user: ")
-
     data = utils.get_link_data(share_link)
 
-    share_link.rstrip()
 
-    print(data)
+    code_URL = "https://www.spotifycodes.com/downloadCode.php?uri=jpeg%2F000000%2Fwhite%2F640%2Fspotify%3A" + data[0] + "%3A" + data[1]
 
-"""
-    if share_link.lstrip("https: // open.spotify.com /").startswith("track"
+    r = requests.get(code_URL)
 
-    https: // open.spotify.com / track / 0
-    oks4FnzhNp5QPTZtoet7c?si = dc6855b6efa54ca0
-
-    https: // open.spotify.com / album / 4
-    m2880jivSbbyEGAKfITCa?si = 1_6
-    htsJURnyVXujlsCiGYw
-    """
+    with open(data[1]+".png", "wb") as fp:
+        fp.write(r.content)
